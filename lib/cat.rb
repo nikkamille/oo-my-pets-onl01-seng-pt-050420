e4eba20c7b9e8c78e65cf5a9d891783b44dc970d
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-02 04:02:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,12 @@
 class Cat
   
   attr_reader :name
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
   
 end

bfea208a04ee367c4324957436caae35ad1d1208
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-02 04:05:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
 class Dog
   
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+  end
+  
   
   
 end

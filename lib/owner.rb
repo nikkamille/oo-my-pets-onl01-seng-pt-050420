2e367fe26c96c27d85e52e0aeb76298e1be47acf
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-02 03:57:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,15 @@
 class Owner
   
-  attr_reader :species
+  attr_reader :species, :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @species = "human"
+  end
+  
+  def self.all 
+    @@all 
+  end
   
 end
